d97c97b3a2c3485cb239ba773b06a1ec7cf3eb92
Kithmini Jayawardena <33926599+fluffykittykitz@users.noreply.github.com>
2018-12-31 06:44:16 +0530

    Login and Register components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -15,11 +14,9 @@ import { LoginComponent } from './login/login.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [
-     // {provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher}
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
